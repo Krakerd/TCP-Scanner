556ab4af115ecd6018e97bef21057d39ef99e840
Krakerd <danielzorlej@gmail.com>
2022-01-15 12:49:56 +0100

    main

--- src/main.go
+++ src/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println("=== TCP SCANNER START ===")
+	/* _, err := net.Dial("tcp", "scanme.nmap.org:80")*/
+	/*if err == nil {*/
+	/*fmt.Println("Connection successful")*/
+	/*}*/
+
+	for i := 1; i <= 1024; i++ {
+		go func(j int) {
+			address := fmt.Sprintf("scanme.nmap.org:%d", j)
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("%d open\n", j)
+		}(i)
+	}
 }
