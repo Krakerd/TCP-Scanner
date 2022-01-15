d34d375ab72f500cc51c430b1761c5f2a0eec44b
Krakerd <danielzorlej@gmail.com>
2022-01-15 12:58:24 +0100

    a

--- src/main.go
+++ src/main.go
@@ -7,20 +7,16 @@ import (
 
 func main() {
 	fmt.Println("=== TCP SCANNER START ===")
-	/* _, err := net.Dial("tcp", "scanme.nmap.org:80")*/
-	/*if err == nil {*/
-	/*fmt.Println("Connection successful")*/
-	/*}*/
 
-	for i := 1; i <= 1024; i++ {
-		go func(j int) {
-			address := fmt.Sprintf("scanme.nmap.org:%d", j)
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
+	for i := 1; i <= 1024; i++ { //create loop of ports to scan
+		go func(j int) { //crea una nueva rutina por cada iteración del bucle
+			address := fmt.Sprintf("scanme.nmap.org:%d", j) // creas la direccion
+			conn, err := net.Dial("tcp", address)           //conectas a la direccion
+			if err != nil {                                 //si hay error pasas al siguiente valor
 				return
 			}
-			conn.Close()
-			fmt.Printf("%d open\n", j)
-		}(i)
+			conn.Close()               //cierras la conexion
+			fmt.Printf("%d open\n", j) //imprimes el valor del puerto
+		}(i) //pasas el valor a la funcion de cada hilo
 	}
 }
